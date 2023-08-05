58986c668bd170bc1311381cd4dedb2fd07a771f
WhitespaceRespecter <bendik.bergsli@gmail.com>
2023-08-05 18:54:00 +0200

    updated functionality

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let firstCard = 4
-let secondCard = 4
-let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-// let cardEl = document.getElementById("cards-el")
 let cardEl = document.querySelector("#cards-el")
+cardEl.textContent += " "
+let cardsArray = []
+let sum = []
 
 function startGame() {
-    cardEl.textContent = "Cards: " + firstCard + " | " + secondCard;
-    sumEl.textContent = "Sum: " + sum;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cardsArray = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardEl.textContent = "Cards: | "
     
+    for (let i = 0; i < cardsArray.length; i++){
+        cardEl.textContent += cardsArray[i] + " | "
+    }
+    
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         messageEl.textContent = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -22,30 +32,31 @@ function startGame() {
     } else {
         messageEl.textContent = "You're out of the game!"
         isAlive = false
+        let nodeList = document.querySelectorAll("button")
+        
+        for (let i = 0; i < nodeList.length; i++){
+            nodeList[i].style.backgroundColor = "#FF0000";
+            nodeList[i].style.color = "#FFFFFF";
+        }
+        
+        document.querySelector("#message-el").style.backgroundColor = "#FF0000";
+        
     }
+
 }
 
 function newCard() {
-    messageEl.textContent = "You received a new car!"
+    messageEl.textContent = "Drawing a new card from the deck!"
     card = getRandomCard(11);
-    cardEl.textContent += " | " + card;
+    cardsArray.push(card)
     sum = sum + card;
-    sumEl.textContent = sum;
 
-        if (sum <= 20) {
-            messageEl.textContent = "Do you want to draw a new card?"
-        } else if (sum === 21) {
-            messageEl.textContent = "Wohoo! You've got Blackjack!"
-            hasBlackJack = true
-        } else {
-            messageEl.textContent = "You're out of the game!"
-            isAlive = false
-        }
+    renderGame()
+    
+    console.log("Card: +", card)
     console.log("Sum: ", sum)
-    console.log("Card: ", card)
 }
 
-function getRandomCard(max) {
-    return Math.ceil(Math.random() * max);
-  }
-  
+function getRandomCard() {
+    return Math.ceil(Math.random() * 11);
+}
